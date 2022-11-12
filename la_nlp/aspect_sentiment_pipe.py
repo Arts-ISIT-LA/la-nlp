8936cac144640a3cd4705854a95c60a633dfd53e
William Strausser <william.strausser@ubc.ca>
2022-11-11 19:17:29 -0800

    Added parent_span to pipeline

--- la_nlp/aspect_sentiment_pipe.py
+++ la_nlp/aspect_sentiment_pipe.py
@@ -12,6 +12,7 @@ def aspect_sentiment_pipe(doc):
     doc = contains_aspect(doc)
     doc = aspects_contained(doc)
     doc = keywords(doc)
+    doc = parent_span(doc)
     return doc
 
 NLP.add_pipe('aspect_sentiment_pipe')
