86cfc1c55d003c1c9a3d75dc630bc43778d3cba1
William Strausser <william.strausser@ubc.ca>
2022-11-11 16:07:40 -0800

    Began writing tests

---
+++ tests/test_aspect_sentiment.py
@@ -0,0 +1,34 @@
+from la_nlp.aspect_sentiment import *
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from spacy.tokens import Doc
+
+
+TEST_TEXT_1 = """
+I enjoyed the course, but the readings were too long and the professor was mean.
+"""
+
+TEST_TEXT_2 = """
+This is a text that does not contain any target aspects.
+"""
+
+def test_make_doc():
+    doc = make_doc(TEST_TEXT_1)
+
+    assertion1 = "Should be a spacy Doc object"
+    assert isinstance(doc, Doc), assertion1
+
+    assertion2 = "Should have a length of 19"
+    assert len(doc) == 19, assertion2
+
+
+def test_contains_aspect():
+    doc1 = make_doc(TEST_TEXT_1)
+    doc2 = make_doc(TEST_TEXT_2)
+
+    assertion1 = "doc1 should return True"
+    assert doc1._.contains_aspect == True
+
+    assertion1 = "doc2 should return False"
+    assert doc2._.contains_aspect == False
