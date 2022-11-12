70c3aa9afb9cd5658ab23ef93578a0733c0b81d8
William Strausser <william.strausser@ubc.ca>
2022-11-11 16:52:14 -0800

    Refactored tests to use pytest fixtures

--- tests/test_aspect_sentiment.py
+++ tests/test_aspect_sentiment.py
@@ -1,4 +1,5 @@
 from la_nlp.aspect_sentiment_pipe import *
+import pytest
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -13,30 +14,32 @@ TEST_TEXT_2 = """
 This is a text that does not contain any target aspects.
 """
 
-def test_make_doc():
-    doc = make_doc(TEST_TEXT_1)
+@pytest.fixture
+def doc1():
+    doc1 = make_doc(TEST_TEXT_1)
+    return doc1
 
+@pytest.fixture
+def doc2():
+    doc2 = make_doc(TEST_TEXT_2)
+    return doc2
+
+def test_make_doc(doc1):
     assertion1 = "Should be a spacy Doc object"
-    assert isinstance(doc, Doc), assertion1
+    assert isinstance(doc1, Doc), assertion1
 
     assertion2 = "Should have a length of 19"
-    assert len(doc) == 19, assertion2
+    assert len(doc1) == 19, assertion2
 
 
-def test_contains_aspect():
-    doc1 = make_doc(TEST_TEXT_1)
-    doc2 = make_doc(TEST_TEXT_2)
-
+def test_contains_aspect(doc1, doc2):
     assertion1 = "doc1 should return True"
     assert doc1._.contains_aspect == True, assertion1
 
     assertion2 = "doc2 should return False"
     assert doc2._.contains_aspect == False, assertion2
 
-def test_aspects_contained():
-    doc1 = make_doc(TEST_TEXT_1)
-    doc2 = make_doc(TEST_TEXT_2)
-
+def test_aspects_contained(doc1, doc2):
     assertion1 = "doc1 should contain 'course', 'content', and 'instructor'"
     doc1_aspects = ['course', 'content', 'instructor']
     assert doc1._.aspects_contained == doc1_aspects, assertion1
@@ -44,3 +47,6 @@ def test_aspects_contained():
     assertion2 = "doc2 should contain None"
     doc2_aspects = None
     assert doc2._.aspects_contained == doc2_aspects, assertion2
+
+def test_aspect_spans(doc1, doc2):
+    assert True
