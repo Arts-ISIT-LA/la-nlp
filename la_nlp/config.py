158284b3d357173082b24d7bbcccf25761615d70
William Strausser <william.strausser@ubc.ca>
2022-11-12 21:11:00 -0800

    Renamed file_name variable to file_path in get_aspects() function

--- la_nlp/config.py
+++ la_nlp/config.py
@@ -6,7 +6,7 @@ PROJECT_DIR = os.path.dirname(FILE_DIR)
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 ASPECT_FILE = os.path.join(DATA_DIR, 'aspects.toml')
 
-def get_aspects(file_name=ASPECT_FILE):
-    with open(file_name, 'rb') as file:
+def get_aspects(file_path=ASPECT_FILE):
+    with open(file_path, 'rb') as file:
         aspects = tomllib.load(file)
     return aspects
