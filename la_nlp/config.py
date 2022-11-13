9c6d9d430022625177df663dabc01ba8e8fc5b96
William Strausser <william.strausser@ubc.ca>
2022-11-12 21:39:55 -0800

    Moved data directory to inside of package

--- la_nlp/config.py
+++ la_nlp/config.py
@@ -3,7 +3,7 @@ import os
 
 FILE_DIR = os.path.dirname(__file__)
 PROJECT_DIR = os.path.dirname(FILE_DIR)
-DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+DATA_DIR = os.path.join(FILE_DIR, 'data')
 ASPECT_FILE = os.path.join(DATA_DIR, 'aspects.toml')
 
 def get_aspects(file_path=ASPECT_FILE):
