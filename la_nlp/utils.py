f05cb27b0e3534f49a318cdee91a691bfa126e7c
William Strausser <william.strausser@ubc.ca>
2022-11-23 12:21:11 -0800

    Wrote docstrings for aspect_sentiment.py and made slight changes to variable names

--- la_nlp/utils.py
+++ la_nlp/utils.py
@@ -6,7 +6,7 @@ PROJECT_DIR = os.path.dirname(FILE_DIR)
 DATA_DIR = os.path.join(FILE_DIR, 'data')
 ASPECT_FILE = os.path.join(DATA_DIR, 'aspects.toml')
 
-def get_aspects_from_file(file_path=ASPECT_FILE):
+def get_aspects_from_file(file_path):
     with open(file_path, 'rb') as file:
         aspects = tomllib.load(file)
     return aspects
@@ -16,3 +16,6 @@ def get_keywords_from_aspects(aspects):
     for keywords_list in aspects.values():
         keywords.extend(keywords_list)
     return keywords
+
+def get_default_aspects():
+    return get_aspects_from_file(ASPECT_FILE)
